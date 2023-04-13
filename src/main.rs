a3f59f65b711b58a4ca46e437d84db1ab46c3b8e
PapyKahan <fajardo.christophe@gmail.com>
2023-04-13 21:33:11 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -74,58 +74,76 @@ fn enumerate_devices() {
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
-    let mut file_path = &String::new();
-    let mut playback_device = rodio::cpal::default_host().default_output_device().unwrap();
-
-    match args.len() {
-        2 => {
-            file_path = &args[1];
-        },
+    let file_path = match args.len() {
+        2 => &args[1],
         _ => {
             println!("Usage: rhap <file>");
             enumerate_devices();
-            return;
+            ""
         },
+    };
+
+    if file_path.is_empty() {
+        return;
     }
 
-    println!("list devices");
-    playback_device = rodio::cpal::default_host().default_output_device().unwrap();
-    //let hosts = rodio::cpal::available_hosts();
-    //for host in hosts {
-    //    let h = rodio::cpal::host_from_id(host).unwrap();
-    //    let devices = h.output_devices().unwrap();
-    //    for device in devices {
-    //        if device.name().unwrap().contains("Qudelix-5K USB DAC") {
-    //            playback_device = device;
-    //            break;
-    //        }
-    //        //if device.name().unwrap().contains("Cayin RU6") {
-    //        //    playback_device = device;
-    //        //    break;
-    //        //}
-    //        //if device.name().unwrap().contains("Realtek High Definition Audio(SST)") {
-    //        //    playback_device = device;
-    //        //    break;
-    //        //}
-    //    }
-    //}
+    let playback_device = match rodio::cpal::default_host().default_output_device() {
+        Some(device) => device,
+        None => {
+            println!("No playback device found");
+            return;
+        }
+    };
+    println!("Using device: {}", playback_device.name().unwrap());
 
     // Configure output stream
-    print!("Configuring output stream...");
-    let config = rodio::cpal::SupportedStreamConfig::new(2, rodio::cpal::SampleRate(44100), rodio::cpal::SupportedBufferSize::Range{ min: 192000, max: 192000 }, rodio::cpal::SampleFormat::I16);
+    let config = rodio::cpal::SupportedStreamConfig::new(
+        2, 
+        rodio::cpal::SampleRate(44100), 
+        rodio::cpal::SupportedBufferSize::Range {
+            min: 44100, 
+            max: 44100
+        }, 
+        rodio::cpal::SampleFormat::I16
+    );
 
     // Get a output stream handle to the selected physical sound device
-    println!("Using device: {}", playback_device.name().unwrap());
-    let (_stream, stream_handle) = rodio::OutputStream::try_from_device_config(&playback_device, config).unwrap();
+    let (_stream, stream_handle) = match rodio::OutputStream::try_from_device_config(&playback_device, config) {
+        Ok((stream, stream_handle)) => (stream, stream_handle),
+        Err(err) => {
+            println!("Error opening output stream: {}", err);
+            return;
+        },
+    };
+
+    let file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Error opening file: {}", err);
+            return;
+        },
+    };
 
     // Load a sound from a file, using a path relative to Cargo.toml
-    let buffer = BufReader::new(File::open(file_path).unwrap());
+    let buffer = BufReader::new(file);
 
     // Decode that sound file into a source
-    let source = Decoder::new(buffer).unwrap();
+    let source = match Decoder::new(buffer) {
+        Ok(decoder) => decoder,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
 
     // Play the sound directly on the device
-    let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+    let sink = match rodio::Sink::try_new(&stream_handle) {
+        Ok(sink) => sink,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
     sink.append(source);
     
     // The sound plays in a separate audio thread,
