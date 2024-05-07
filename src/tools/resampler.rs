22b31d3192bc00f6c8bae50d54d2d6c0218665d6
Christophe Fajardo <christophe.fajardo@swisscaution.ch>
2024-05-07 13:23:22 +0200

    chore(wasapi): tests

--- src/tools/resampler.rs
+++ src/tools/resampler.rs
@@ -6,41 +6,37 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
 
+use rubato::{calculate_cutoff, Resampler, SincInterpolationParameters, SincInterpolationType, WindowFunction};
 use symphonia::core::audio::{AudioBuffer, AudioBufferRef, Signal, SignalSpec};
 use symphonia::core::conv::{FromSample, IntoSample};
 use symphonia::core::sample::Sample;
-use rubato::Resampler;
 
+use rubato::sinc_interpolator::sinc_interpolator_avx::AvxInterpolator;
 
 pub struct ResamplerUtil<T> {
-    resampler: rubato::FftFixedIn<f32>,
-    //resampler: rubato::FastFixedIn<f32>,
-    //resampler: rubato::SincFixedIn<f32>,
-    input: Vec<Vec<f32>>,
-    output: Vec<Vec<f32>>,
+    resampler: rubato::FftFixedIn<f64>,
+    //resampler: rubato::FastFixedIn<f64>,
+    //resampler: rubato::SincFixedIn<f64>,
+    input: Vec<Vec<f64>>,
+    output: Vec<Vec<f64>>,
     interleaved: Vec<T>,
     duration: usize,
 }
 
-
 impl<T> ResamplerUtil<T>
 where
-    T: Sample + FromSample<f32> + IntoSample<f32>,
+    T: Sample + FromSample<f64> + IntoSample<f64>,
 {
     fn resample_inner(&mut self) -> &[T] {
         {
-            let mut input: arrayvec::ArrayVec<&[f32], 32> = Default::default();
+            let mut input: arrayvec::ArrayVec<&[f64], 32> = Default::default();
             for channel in self.input.iter() {
                 input.push(&channel[..self.duration]);
             }
 
-            // Resample.
-            self.resampler.process_into_buffer(
-                &input,
-                &mut self.output,
-                None,
-            )
-            .unwrap();
+            self.resampler
+                .process_into_buffer(&input, &mut self.output, None)
+                .unwrap();
         }
 
         // Remove consumed samples from the input buffer.
@@ -51,7 +47,8 @@ where
         // Interleave the planar samples from Rubato.
         let num_channels = self.output.len();
 
-        self.interleaved.resize(num_channels * self.output[0].len(), T::MID);
+        self.interleaved
+            .resize(num_channels * self.output[0].len(), T::MID);
 
         for (i, frame) in self.interleaved.chunks_exact_mut(num_channels).enumerate() {
             for (ch, s) in frame.iter_mut().enumerate() {
@@ -65,19 +62,20 @@ where
 
 impl<T> ResamplerUtil<T>
 where
-    T: Sample + FromSample<f32> + IntoSample<f32>,
+    T: Sample + FromSample<f64> + IntoSample<f64>,
 {
     pub fn new(spec: &SignalSpec, to_sample_rate: usize, duration: u64) -> Self {
         let duration = duration as usize;
         let num_channels = 2;
 
-        //let ratio = to_sample_rate as f32 / spec.rate as f32;
+        //let ratio = to_sample_rate as f64 / spec.rate as f64;
         //let sinc_len = 256;
-        //let oversampling_factor = 128;
-        //let interpolation = SincInterpolationType::Cubic;
+        //let oversampling_factor = sinc_len;
+        //let interpolation_type = SincInterpolationType::Nearest;
         //let window = WindowFunction::BlackmanHarris2;
 
         //let f_cutoff = calculate_cutoff(sinc_len, window);
+        //let f_cutoff = 0.80;
         //let params = SincInterpolationParameters {
         //    sinc_len,
         //    f_cutoff,
@@ -85,21 +83,25 @@ where
         //    oversampling_factor,
         //    window,
         //};
-        //let resampler = rubato::SincFixedIn::<f32>::new(ratio, 1.0, params, duration, num_channels).unwrap();
 
-        //let resampler = rubato::FastFixedIn::<f32>::new(ratio, 1.1, rubato::PolynomialDegree::Quintic, duration, num_channels).unwrap();
+        //let resampler = rubato::FastFixedIn::<f64>::new(ratio, 1.1, rubato::PolynomialDegree::Quintic, duration, num_channels).unwrap();
 
-        let subchunk_size = if spec.rate as usize > to_sample_rate {
-            duration / (spec.rate as usize / to_sample_rate)
-        } else {
-            duration / (to_sample_rate / spec.rate as usize)
-        };
+        //let subchunk_size = if spec.rate as usize > to_sample_rate {
+        //    duration / (spec.rate as usize / to_sample_rate)
+        //} else {
+        //    duration / (to_sample_rate / spec.rate as usize)
+        //};
+        
+        //let resampler = rubato::SincFixedIn::<f64>::new(ratio, 1.0, params, duration, num_channels).unwrap();
+        //let avx = AvxInterpolator::new(sinc_len, oversampling_factor, f_cutoff, window).unwrap();
+        //let interpolator = Box::new(avx);
+        //let resampler = rubato::SincFixedIn::<f64>::new_with_interpolator(ratio, 1.1, interpolation_type, interpolator, duration, num_channels).unwrap();
 
-        let resampler = rubato::FftFixedIn::<f32>::new(
+        let resampler = rubato::FftFixedIn::<f64>::new(
             spec.rate as usize,
             to_sample_rate,
             duration,
-            subchunk_size,
+            duration,
             num_channels,
         )
         .unwrap();
@@ -145,7 +147,7 @@ where
             // Fill each input channel buffer with silence to the next multiple of the resampler
             // duration.
             for channel in self.input.iter_mut() {
-                channel.resize(len + (self.duration - partial_len), f32::MID);
+                channel.resize(len + (self.duration - partial_len), f64::MID);
             }
         }
 
@@ -153,7 +155,7 @@ where
     }
 }
 
-fn convert_samples_any(input: &AudioBufferRef<'_>, output: &mut [Vec<f32>]) {
+fn convert_samples_any(input: &AudioBufferRef<'_>, output: &mut [Vec<f64>]) {
     match input {
         AudioBufferRef::U8(input) => convert_samples(input, output),
         AudioBufferRef::U16(input) => convert_samples(input, output),
@@ -168,9 +170,9 @@ fn convert_samples_any(input: &AudioBufferRef<'_>, output: &mut [Vec<f32>]) {
     }
 }
 
-fn convert_samples<S>(input: &AudioBuffer<S>, output: &mut [Vec<f32>])
+fn convert_samples<S>(input: &AudioBuffer<S>, output: &mut [Vec<f64>])
 where
-    S: Sample + IntoSample<f32>,
+    S: Sample + IntoSample<f64>,
 {
     for (c, dst) in output.iter_mut().enumerate() {
         let src = input.chan(c);
